2f35df5411d38e0300e6f93f71f3d7a8bf4fed96
yashika-sharma <yashikasharma.m@gmail.com>
2021-10-19 15:44:45 +0530

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+//global variable
+let data = [];
+
+function fetchData() {
+	fetch("data.json")
+		.then(function (response) {
+			return response.json();
+		})
+		.then((res) => {
+			data = res;
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+}
+
+window.onload = fetchData();
+
 function toggleView(state) {
-	const e = document.getElementsByClassName("current");
-	console.log(e);
-	data.forEach((item) => {});
+	data.forEach((item) => {
+		document.getElementById(`current-${item.title.toLowerCase()}`).innerHTML =
+			item.timeframes[state].current + "hrs";
+		let previousValue =
+			state === "weekly"
+				? "Last Week"
+				: state === "daily"
+				? "Yesterday"
+				: "Last Month";
+		document.getElementById(`previous-${item.title.toLowerCase()}`).innerHTML =
+			previousValue + " - " + item.timeframes[state].previous + "hrs";
+	});
 }
 
 function handleWeekly() {
@@ -15,10 +42,12 @@ function handleDaily() {
 	document.getElementById("weekly").className = "inactive";
 	document.getElementById("daily").className = "active";
 	document.getElementById("monthly").className = "inactive";
+	toggleView("daily");
 }
 
 function handleMonthly() {
 	document.getElementById("weekly").className = "inactive";
 	document.getElementById("daily").className = "inactive";
 	document.getElementById("monthly").className = "active";
+	toggleView("monthly");
 }
